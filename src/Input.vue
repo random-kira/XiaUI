c1451dcac6022a5947e5e521e101f2c81bbbe862
random_fe <564030084@qq.com>
2021-08-19 17:36:33 +0800

    add Input.vue style done & update svg

---
+++ src/Input.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="wrapper" :class="{error}">
+        <input :value="value" type="text" :disabled="disabled" :readonly="readonly">
+        <template v-if="error">
+            <icon name="error" class="inputIcon"></icon>
+            <span>{{error}}</span>
+        </template>
+    </div>
+</template>
+
+<script>
+  import Icon from "./Icon"
+  export default {
+    name: 'Input',
+    components: {Icon},
+    props:{
+      value:{
+        type: String
+      },
+      disabled:{
+        type: Boolean,
+        default: false
+      },
+      readonly:{
+        type: Boolean,
+        default: false
+      },
+      error:{
+        type: String
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover:#666;
+    $border-radius:4px;
+    $font-size:14px;
+    $box-shadow-color:rgba(0,0,0,0.5);
+    .wrapper{
+        font-size: $font-size;
+        display: inline-block;
+        >.inputIcon{
+            vertical-align: middle;
+        }
+        > input{
+            height: $height;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            font-size: inherit;
+            padding: 0 8px;
+            &:hover{
+                border-color: $border-color-hover;
+            }
+            &:focus{
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none;
+            }
+            &[disabled], &[readonly]{
+                border-color: #bbb;
+                color: #bbb;
+                cursor: not-allowed;
+            }
+        }
+        &.error{
+            margin-right: .1em;
+            &:last-child{
+                margin-right: 0;
+            }
+            >input{
+              border-color: #FA5151;
+            }
+        }
+    }
+</style>
--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Button from "./Button"
 import Icon from "./Icon"
-import  ButtonGroup from "./Button-group"
+import ButtonGroup from "./Button-group"
+import Input from "./Input"
 
 Vue.component('xia-button', Button)
 Vue.component('xia-icon', Icon)
 Vue.component('xia-button-group',ButtonGroup)
+Vue.component('xia-input',Input)
 
 new Vue({
     el:'#app',
