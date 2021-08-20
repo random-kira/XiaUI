ba7fa0564f5ba72d782d0f7d451ab0f46078babe
random_fe <564030084@qq.com>
2021-08-20 20:57:21 +0800

    add toast.vue & as it  plugin

--- src/app.js
+++ src/app.js
@@ -5,6 +5,13 @@ import ButtonGroup from "./Button-group"
 import Input from "./Input"
 import Col from "./Col"
 import Row from "./Row"
+import Layout from "./Layout"
+import Sider from "./Sider"
+import Content from "./Content"
+import Footer from "./Footer"
+import Header from "./Header"
+import Toast from "./Toast"
+import plugin from "./plugin"
 
 Vue.component('xia-button', Button)
 Vue.component('xia-icon', Icon)
@@ -13,7 +20,22 @@ Vue.component('xia-input',Input)
 Vue.component('xia-col',Col)
 Vue.component('xia-row',Row)
 
+Vue.component('xia-layout',Layout)
+Vue.component('xia-sider',Sider)
+Vue.component('xia-content',Content)
+Vue.component('xia-footer',Footer)
+Vue.component('xia-header',Header)
+
+Vue.component('xia-toast',Toast)
+Vue.use(plugin)
+
+
 new Vue({
     el:'#app',
+    methods:{
+      showToast(message){
+        this.$toast(message)
+      }
+    }
   }
 )
