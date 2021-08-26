746c1f9654c5e0bf720be2884196ca278a728517
random_fe <564030084@qq.com>
2021-08-26 20:27:35 +0800

    add collapse.vue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Icon from "./src/Icon"
 import Button from "./src/Button"
-import ButtonGroup from "./src/Button-group"
+import ButtonGroup from "./src/Button-Group"
 
 export {Icon, Button, ButtonGroup}
